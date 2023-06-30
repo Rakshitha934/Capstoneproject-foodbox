6b7c7330b80062585e16f5d8e3a8b843ba1daa75
Rakshitha934 <rakshitha934@gmail.com>
2023-07-01 03:53:09 +0530

    frontend commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { NgModule } from '@angular/core';
+import { ProductComponent } from './product/product.component';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
+import { MatSortModule } from '@angular/material/sort';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ManageCustomersComponent } from './manage-customers/manage-customers.component';
+import { ManagePurchaseComponent } from './manage-purchase/manage-purchase.component';
+import { PaymentGatewayComponent } from './payment-gateway/payment-gateway.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    CartComponent,
+    AdminLoginComponent,
+    AdminDashboardComponent,
+    ManageCustomersComponent,
+    ManagePurchaseComponent,
+    PaymentGatewayComponent,
+    OrderSummaryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatSortModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/manage-customers/manage-customers.component.ts
@@ -0,0 +1,71 @@
+import { Router } from '@angular/router';
+import { LogService } from '../log.service';
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
+import { Purchase } from '../purchase';
+import { PurchaseService } from '../purchase.service';
+
+
+@Component({
+  selector: 'app-manage-customers',
+  templateUrl: './manage-customers.component.html',
+  styleUrls: ['./manage-customers.component.css']
+})
+export class ManageCustomersComponent implements OnInit {
+  searchKey:string;
+  public allCustomers:Customer[];
+  public activeOrders:Purchase[]=[];
+  constructor(private customerService:CustomerService,
+    private purchaseService:PurchaseService,
+    private logService:LogService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+    this.logService.loggedAdminId$.subscribe((id)=>{
+      console.log('id '+id);
+      if(id==''){
+        this.router.navigate(['/admin']);
+      }
+    })
+    this.getCustomers();
+  }
+
+  getCustomers(){
+    this.customerService.getCustomers().subscribe(data=>{
+      this.allCustomers=data;
+    })
+  }
+
+  adminLogout(){
+    this.logService.sendId('');
+    this.router.navigate(['/admin']);
+  }
+
+  searchCustomer(){
+    if(this.searchKey==''){
+      this.getCustomers();
+    }else{
+      this.customerService.searchCustomer(this.searchKey).subscribe(data=>{
+        this.allCustomers=data;
+      })
+    }
+  }
+
+  getActiveOrders(email:string){
+    this.purchaseService.getCustomerOrders(email).subscribe(data=>{
+      this.activeOrders=data;
+    })
+  }
+
+  deleteCustomer(email:string){
+    this.customerService.deleteCustomer(email).subscribe(data=>{
+      alert("Customer Deleted");
+      this.getCustomers();
+    },error=>{
+      alert("Customer can't be deleted until their orders are deleted")
+    }
+    )
+  }
+
+}
